e7c9fe35f55a5841f5f69d187011e5abc7070465
Yang Liu <liuyangzhuan@gmail.com>
2024-03-19 14:09:21 -0700

    added VIE2D in addition to VIE3D

--- cross/test_vie2d.m
+++ cross/test_vie2d.m
@@ -1,25 +1,24 @@
 
 clear all
-fun=@vie_green3d;
+fun=@vie_green2d;
 global wavelen ppw
-
-% wavelens = [0.001953125 0.00390625 0.0078125 0.015625 0.03125 0.0625];
-wavelens = [0.0078125 0.00390625 0.001953125];
+% wavelens = [0.00012207031 0.00024414062 0.00048828125 0.0009765625 0.001953125 0.00390625 0.0078125 0.015625];
+wavelens = [0.00048828125 0.00024414062 0.00012207031];
 for ii = 1:size(wavelens,2)
 wavelen=wavelens(ii);
 ppw=4.0;
+tol=1e-6;
 ds = wavelen/ppw;
 N=round(1/ds);
-outputfile=['QTT_VIE3D_admissible_N_',num2str(N),'.txt'];
+outputfile=['QTT_VIE2D_admissible_N_',num2str(N),'_tol_',num2str(tol),'.txt'];
 if exist(outputfile, 'file')==2
     delete(outputfile);
 end
 diary(outputfile) 
 disp(["N=",num2str(N)])
-tol=1e-2;
 % N=8192;
 s = log2(N);
-d=3*s;
+d=2*s;
 inp=zeros(d,1);
 inp(:)=4;
 tic;
--- cross/vie_green2d.m
+++ cross/vie_green2d.m
@@ -1,36 +1,34 @@
-function y=vie_green3d(x)
+function y=vie_green2d(x)
     global wavelen ppw
-    d=max(size(x,2),size(x,1))/3;
+    d=max(size(x,2),size(x,1))/2;
     ds = wavelen/ppw;
     waven=2*pi/wavelen;
     % ds=0.01;
-    zdist=2;
+    zdist=1;
 
     xxs=x(1:d);
     yys=x(d+1:2*d);
-    zzs=x(2*d+1:3*d);
 
-%     xxs=x(1:3:3*d);
-%     yys=x(2:3:3*d);
-%     zzs=x(3:3:3*d);
+%     xxs=x(1:2:2*d);
+%     yys=x(2:2:2*d);
 
     
     xs=0;
     ys=0;
-    zs=0;
+    % zs=0;
     xo=0;
     yo=0;
-    zo=0;    
+    % zo=0;    
     for ii=1:d
       xo = xo + mod(xxs(ii)-1,2)*2^(ii-1);  
       xs = xs + floor((xxs(ii)-1)/2)*2^(ii-1);  
       yo = yo + mod(yys(ii)-1,2)*2^(ii-1);  
       ys = ys + floor((yys(ii)-1)/2)*2^(ii-1);  
-      zo = zo + mod(zzs(ii)-1,2)*2^(ii-1);  
-      zs = zs + floor((zzs(ii)-1)/2)*2^(ii-1);              
+    %   zo = zo + mod(zzs(ii)-1,2)*2^(ii-1);  
+    %   zs = zs + floor((zzs(ii)-1)/2)*2^(ii-1);              
     end
-    oo = [xo*ds, yo*ds, zo*ds];
-    ss = [xs*ds, ys*ds, zs*ds+zdist];
+    oo = [xo*ds, yo*ds, 0];
+    ss = [xs*ds, ys*ds, zdist];
     r =sqrt(sum((oo-ss).^2));
     if(r~=0)
 %         y=1/r;  % static kernel
