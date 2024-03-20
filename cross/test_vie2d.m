ef22b74f637498838ee7f57d715dacf257e22066
Yang Liu <liuyangzhuan@gmail.com>
2024-03-20 00:16:03 -0700

    added mat-vec test for a random input vector

--- cross/test_vie2d.m
+++ cross/test_vie2d.m
@@ -2,8 +2,9 @@
 clear all
 fun=@vie_green2d;
 global wavelen ppw
+testmatvec=1;
 % wavelens = [0.00012207031 0.00024414062 0.00048828125 0.0009765625 0.001953125 0.00390625 0.0078125 0.015625];
-wavelens = [0.00048828125 0.00024414062 0.00012207031];
+wavelens = [0.0078125];
 for ii = 1:size(wavelens,2)
 wavelen=wavelens(ii);
 ppw=4.0;
@@ -24,12 +25,33 @@ inp(:)=4;
 tic;
 [y]=amen_cross(inp, fun, tol);
 % tt=dmrg_cross(d,inp,fun,tol);
+disp('time/rank/memory for the VIE operator:')
 toc
 y1=round(y,tol);
 % norm(y1-y)/norm(y)
 
-maxrank = max(rank(y1))
-memory=mem(y1)*16/1e9
+maxrank_A = max(rank(y1))
+memory_A=mem(y1)*16/1e9
+
+
+
+
+%% unfold the tt_tensor into tt_matrix and test mat-vec with a random input vector (whose TT rank is sqrt(N^0.5))
+if(testmatvec==1)
+    tmat=tt_matrix(y1,2,2);
+    dims=[];
+    for i=1:d
+        dims=[dims,2];
+    end
+    brand = randn(dims);
+    tb=tt_tensor(brand,tol);
+    tic;
+    tc = tmat*tb;
+    disp('time/rank/memory for the matvec:')
+    toc
+    maxrank_c = max(rank(tc))
+    memory_c=mem(tc)*16/1e9
+end
 diary off
 end
 
--- cross/test_vie3d.m
+++ cross/test_vie3d.m
@@ -2,9 +2,9 @@
 clear all
 fun=@vie_green3d;
 global wavelen ppw
-
+testmatvec=1;
 % wavelens = [0.001953125 0.00390625 0.0078125 0.015625 0.03125 0.0625];
-wavelens = [0.0078125 0.00390625 0.001953125];
+wavelens = [0.0625];
 for ii = 1:size(wavelens,2)
 wavelen=wavelens(ii);
 ppw=4.0;
@@ -25,12 +25,33 @@ inp(:)=4;
 tic;
 [y]=amen_cross(inp, fun, tol);
 % tt=dmrg_cross(d,inp,fun,tol);
+disp('time/rank/memory for the VIE operator:')
 toc
 y1=round(y,tol);
 % norm(y1-y)/norm(y)
 
-maxrank = max(rank(y1))
-memory=mem(y1)*16/1e9
+maxrank_A = max(rank(y1))
+memory_A=mem(y1)*16/1e9
+
+
+
+%% unfold the tt_tensor into tt_matrix and test mat-vec with a random input vector (whose TT rank is sqrt(N^0.5))
+if(testmatvec==1)
+    tmat=tt_matrix(y1,2,2);
+    dims=[];
+    for i=1:d
+        dims=[dims,2];
+    end
+    brand = randn(dims);
+    tb=tt_tensor(brand,tol);
+    tic;
+    tc = tmat*tb;
+    disp('time/rank/memory for the matvec:')
+    toc
+    maxrank_c = max(rank(tc))
+    memory_c=mem(tc)*16/1e9
+end
+
 diary off
 end
 
